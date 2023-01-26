d329143b0fd7fb72ad94b173e5c7ca55344d101e
JoaoHypo <jguibarboza@gmail.com>
2023-01-26 00:02:59 -0300

    Added Sorteio, frist code from my 30th streak day!

---
+++ Sorteio.py
@@ -0,0 +1,42 @@
+'''
+Sorteio
+Todo: Add text
+'''
+
+import random
+random.seed(1024)
+pontos1 = []
+pontos2 = []
+while True:
+    jogador1 = []
+    jogador2 = []
+    sorteados = []
+    while len(jogador2) < 5:
+        numero = int(input())
+        if numero == 0: break   
+        if 0 < numero <= 20:
+            if len(jogador1) < 5:
+                jogador1.append(numero)
+            elif len(jogador2) < 5:
+                jogador2.append(numero)               
+    if numero == 0: break   
+    while len(sorteados) < 5:
+        sorteado = random.randint(1, 20)
+        if sorteado not in sorteados:            
+            sorteados.append(sorteado)
+    
+    ponto1Rodada = (len(set(jogador1).intersection(sorteados)) * 10)
+    pontos1.append(ponto1Rodada)
+
+    ponto2Rodada = (len(set(jogador2).intersection(sorteados)) * 10)        
+    pontos2.append(ponto2Rodada)    
+    
+    print(f"JOGADOR 1 = {ponto1Rodada}, JOGADOR 2 = {ponto2Rodada}")
+
+
+if sum(pontos1) > sum(pontos2):
+    print("JOGADOR 1 VENCEU!")
+elif sum(pontos1) < sum(pontos2):
+    print("JOGADOR 2 VENCEU!")
+else:
+    print("EMPATE!")
