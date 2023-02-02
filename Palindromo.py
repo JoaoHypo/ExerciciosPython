14068b624623924570863f3e0bab5953ac2841b3
João "Hypo" Barboza <jguibarboza@gmail.com>
2023-02-01 22:08:42 -0300

    palindrome exc ptbr

---
+++ Palindromo.py
@@ -0,0 +1,21 @@
+'''
+Palindromo PTBR
+
+Um palíndromo é uma palavra, ou frase, que é igual quando lida da esquerda para a direita ou da direita para a esquerda (espaços em brancos não são considerados). Escreva um programa que leia uma string inserida pelo usuário e imprima, como saída, ‘É palíndromo’ caso a string seja um palíndromo e ‘Não é palíndromo’ caso não seja.
+Input
+Uma string que pode ser ou não um palíndromo.
+Output
+Uma mensagem indicando se a string informada é ou não um palíndromo.
+
+Samples Input	Samples Output
+
+mega bobagem
+É palíndromo
+
+roma
+Não é palíndromo
+
+reviver
+É palíndromo
+'''
+
