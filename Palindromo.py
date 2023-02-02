129088d27d62ab60c27e087af3f0efa3f720c2a5
João "Hypo" Barboza <jguibarboza@gmail.com>
2023-02-02 12:22:42 -0300

    String palindrome - beecrowd need fix

--- Palindromo.py
+++ Palindromo.py
@@ -1,7 +1,10 @@
 '''
 Palindromo PTBR
 
-Um palíndromo é uma palavra, ou frase, que é igual quando lida da esquerda para a direita ou da direita para a esquerda (espaços em brancos não são considerados). Escreva um programa que leia uma string inserida pelo usuário e imprima, como saída, ‘É palíndromo’ caso a string seja um palíndromo e ‘Não é palíndromo’ caso não seja.
+Um palíndromo é uma palavra, ou frase, que é igual quando lida da esquerda
+para a direita ou da direita para a esquerda (espaços em brancos não são considerados).
+Escreva um programa que leia uma string inserida pelo usuário e imprima, como saída,
+‘É palíndromo’ caso a string seja um palíndromo e ‘Não é palíndromo’ caso não seja.
 Input
 Uma string que pode ser ou não um palíndromo.
 Output
@@ -18,4 +21,28 @@ Não é palíndromo
 reviver
 É palíndromo
 '''
+string = input()
+string = string.split()
+result = []
 
+for palavra in string:
+    if len(palavra)%2 == 0:
+        pass
+    else:
+        meio = int(len(palavra)/2)
+        palavra = palavra[ :meio] + palavra[(meio+1): ]
+    normal = []
+    reverso = []
+    for letra in palavra:
+        normal.append(letra)
+        reverso.insert(0,letra)
+    if normal == reverso:
+        pass
+    else:
+        result.append(0)
+
+if 0 in result:
+    print('Não é palíndromo')
+
+else:
+    print('É palíndromo')
