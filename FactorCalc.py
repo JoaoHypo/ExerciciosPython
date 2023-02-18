62913cc08d8d6c7d70225849b3ab089c47e32e31
JoaoHypo <jguibarboza@gmail.com>
2023-02-18 13:45:33 -0300

    Lil progress, added objetctives and todos

--- FactorCalc.py
+++ FactorCalc.py
@@ -1,4 +1,5 @@
 # Factor calculator for ax² such as a=1
+#Todo = add menu, tipes of factorin, and converstion to fractions! (note: split('/'))
 
 nX = float(input('Digite o termo X: '))
 nC = float(input('Digite o termo Cte: '))
@@ -8,4 +9,5 @@ for valor in range(-nC,nC+1):
     valor2 = nC/valor
     print(f'{valor}\t{valor2}\t{nC}')
     if valor2 + valor == nX:
+        print(f'The factoring for x² + {nX}x + {nC}')
         pass
