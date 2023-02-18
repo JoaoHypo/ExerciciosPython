7419ff97330abffb9fe22ebb4c53b5d93974d865
JoaoHypo <jguibarboza@gmail.com>
2023-02-18 13:49:18 -0300

    test

--- FactorCalc.py
+++ FactorCalc.py
@@ -4,10 +4,11 @@
 nX = float(input('Digite o termo X: '))
 nC = float(input('Digite o termo Cte: '))
 
-print(f'Multiplo 1\tMultiplo2\tCte={nC}')
+print(f'Multiplo 1\tMultiplo2\tCte={nC:.0f}')
 for valor in range(-nC,nC+1):
     valor2 = nC/valor
-    print(f'{valor}\t{valor2}\t{nC}')
+    print(f'{valor:.0f}\t{valor2:.0f}\t{nC:.0f}')
     if valor2 + valor == nX:
-        print(f'The factoring for x² + {nX}x + {nC}')
-        pass
+        print(f'The factoring for x² + {nX:.0f}x + {nC:.0f}')
+        print(f'is (x + {valor2:.0f}) * (x + {valor:.0f})')
+        break
