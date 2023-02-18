e38619ca2bda2dec84f1decafc046c63c9e4b1a0
JoaoHypo <jguibarboza@gmail.com>
2023-02-18 13:56:37 -0300

    working, need to optimize and add other options

--- FactorCalc.py
+++ FactorCalc.py
@@ -1,13 +1,13 @@
 # Factor calculator for ax² such as a=1
 #Todo = add menu, tipes of factorin, and converstion to fractions! (note: split('/'))
 
-nX = float(input('Digite o termo X: '))
-nC = float(input('Digite o termo Cte: '))
+nX = int(input('Digite o termo X: '))
+nC = int(input('Digite o termo Cte: '))
 
 print(f'Multiplo 1\tMultiplo2\tCte={nC:.0f}')
 for valor in range(-nC,nC+1):
     valor2 = nC/valor
-    print(f'{valor:.0f}\t{valor2:.0f}\t{nC:.0f}')
+    print(f'{valor}\t\t{valor2}\t\t{nC}')
     if valor2 + valor == nX:
         print(f'The factoring for x² + {nX:.0f}x + {nC:.0f}')
         print(f'is (x + {valor2:.0f}) * (x + {valor:.0f})')
