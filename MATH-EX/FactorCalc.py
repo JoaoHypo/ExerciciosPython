35ce1ae54b84460b3f4e2f4d7468f8516da31742
JoaoHypo <jguibarboza@gmail.com>
2023-02-18 19:30:18 -0300

    more progress

--- MATH-EX/FactorCalc.py
+++ MATH-EX/FactorCalc.py
@@ -3,6 +3,7 @@
 nX2= int(input('Digite o fator X²: '))
 nX = int(input('Digite o faotr X: '))
 nC = int(input('Digite o termo C(te): '))
+find = False
 
 if nX2 == 1: 
     print(f'''\n{'"Brute Force" try process':-^50}''')
@@ -16,8 +17,28 @@ if nX2 == 1:
         if valor2 + valor == nX:
             print(f'\nThe factoring for x² + {nX:.0f}x + {nC:.0f}')
             print(f'is (x + {valor2:.0f}) * (x + {valor:.0f})\n')
+            find = True
             break
-        elif valor == nC:
-            print(f'\nImpossible to factor with integer factors only the expression: x² + {nX:.0f}x + {nC:.0f}')
+    if find == False:
+        print(f'\nImpossible to factor with integer factors only the expression: x² + {nX:.0f}x + {nC:.0f}')
+
 elif nX2 > 1:
+    nX2AUX = nX2*nC
+    print(f'''\n{'"Brute Force" try process':-^50}''')
+    print(f'Multiplo 1\tMultiplo2\tSum Expected= {nX:.0f}')
+
+    for valor in range(-abs(nX2AUX),abs(nX2AUX+1)):
+        if valor == 0:
+            continue
+        valor2 = nX2AUX/valor
+        print(f'\n{valor:^10.2f}\t{valor2:^10.2f}\t{valor2 + valor:^10.2f}')
+        if valor2 + valor == nX:
+            print(f'\nThe factoring for x² + {nX:.0f}x + {nC:.0f} is: ')
+            print(f'(x + {valor2:.0f}) * (x + {valor:.0f})\n')
+            find = True
+            break
+    if find == False:
+        print(f'\nImpossible to factor with integer factors only the expression: {nX2:.0f}x² + {nX:.0f}x + {nC:.0f}')
+
+
     pass
