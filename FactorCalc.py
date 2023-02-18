9cb6653bb554e6f1bce91ba95e7d6967f1c62e1c
JoaoHypo <jguibarboza@gmail.com>
2023-02-18 18:00:00 -0300

    beautiful

--- FactorCalc.py
+++ FactorCalc.py
@@ -4,11 +4,13 @@
 nX = int(input('Digite o termo X: '))
 nC = int(input('Digite o termo Cte: '))
 
-print(f'Multiplo 1\tMultiplo2\tCte={nC:.0f}')
+print(f'''\n{'"Brute Force" try process':-^50}''')
+print(f'Multiplo 1\tMultiplo2\tSum Expected= {nX:.0f}')
+
 for valor in range(-nC,nC+1):
     valor2 = nC/valor
-    print(f'{valor}\t\t{valor2}\t\t{nC}')
+    print(f'\n{valor:^10.2f}\t{valor2:^10.2f}\t{valor2 + valor:^10.2f}')
     if valor2 + valor == nX:
-        print(f'The factoring for x² + {nX:.0f}x + {nC:.0f}')
-        print(f'is (x + {valor2:.0f}) * (x + {valor:.0f})')
+        print(f'\nThe factoring for x² + {nX:.0f}x + {nC:.0f}')
+        print(f'is (x + {valor2:.0f}) * (x + {valor:.0f})\n')
         break
